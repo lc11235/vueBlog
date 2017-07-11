0ddc463e55b967a73f206eeb365dd45bed27d155
lc11235 <lc11235@qq.com>
2017-07-12 00:58:11 +0800

    add response time

--- server/api.js
+++ server/api.js
@@ -5,8 +5,8 @@ const cryptoMd5 = require('./tool/cryptoMd5.js');
 const fn = () => { };
 
 router.get('/api/getArticle', (req, res) => {
-    const _id = req.query.id;
     const start = new Date().getTime();
+    const _id = req.query.id;
     db.Article.findOne({ _id }, (err, doc) => {
         if (err) {
             console.log(err);
@@ -19,19 +19,25 @@ router.get('/api/getArticle', (req, res) => {
 });
 
 router.get('/api/getArticles', (req, res) => {
+    const start = new Date().getTime();
     db.Article.find(null, 'articleTitle author postTime content', (err, doc) => {
         if (err) {
             console.log(err);
         } else if (doc) {
+            const execTime = new Date().getTime() - start;
+            res.set('X-Response-Time', `${execTime}ms`);
             res.send(JSON.stringify(doc));
         }
     });
 });
 
 router.post('/api/login', (req, res) => {
+    const start = new Date().getTime();
     let { name, pwd } = req.body;
     db.User.findOne({ name }, 'pwd', (err, doc) => {
         pwd = cryptoMd5(pwd);
+        const execTime = new Date().getTime() - start;
+        res.set('X-Response-Time', `${execTime}ms`);
         switch (true) {
             case !!err:
                 console.log(err);
@@ -53,6 +59,7 @@ router.post('/api/login', (req, res) => {
 });
 
 router.post('/api/saveArticle', (req, res) => {
+    const start = new Date().getTime();
     const id = req.body._id;
     // TODO：加入最后更新时间
     const article = {
@@ -68,25 +75,35 @@ router.post('/api/saveArticle', (req, res) => {
         new db.Article(article).save();
     }
 
+    const execTime = new Date().getTime() - start;
+    res.set('X-Response-Time', `${execTime}ms`);
     res.status(200).end();
 });
 
 router.post('/api/deleteArticle', (req, res) => {
+    const start = new Date().getTime();
     db.Article.findByIdAndRemove(req.body.id, fn);
+    const execTime = new Date().getTime() - start;
+    res.set('X-Response-Time', `${execTime}ms`);
     res.status(200).end();
 });
 
 router.post('/api/getLinks', (req, res) => {
+    const start = new Date().getTime();
     db.Link.find(null, (err, doc) => {
         if(err){
             console.log(err);
         } else if(doc){
+            const execTime = new Date().getTime() - start;
+            res.set('X-Response-Time', `${execTime}ms`);
             res.send(doc);
         }
     });
 });
 
 router.post('/api/saveLinks', (req, res) => {
+    //const start = new Date().getTime();
+    // todo
     const links = req.body || [];
     db.Link.remove(null, fn);
     const promises = links.map(({name, href}) => new db.Link({name, href}).save());
@@ -96,9 +113,12 @@ router.post('/api/saveLinks', (req, res) => {
 });
 
 router.post('/api/savePwd', (req, res) => {
+    const start = new Date().getTime();
     let {name, pwd} = req.body;
     pwd = cryptoMd5(pwd);
     db.User.findOneAndUpdate({name}, {pwd}, fn);
+    const execTime = new Date().getTime() - start;
+    res.set('X-Response-Time', `${execTime}ms`);
     res.status(200).end();
 });
 
